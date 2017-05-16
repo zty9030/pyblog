d0c4be74c10ed0327d98034438f83ad2dc61c0ff
Williamz <zty9030@hotmail.com>
2017-05-16 10:36:32 -0400

    Update watson_tone.py

--- tools/watson_tone.py
+++ tools/watson_tone.py
@@ -3,8 +3,8 @@ from watson_developer_cloud import ToneAnalyzerV3
 
 def tone(te):
 	tone_analyzer = ToneAnalyzerV3(
-   		password =  "FwdYI5O0TaGY",
-   		username =  "be8de89c-f720-43eb-beec-9f52093bdb6a",
+   		password =  "",
+   		username =  "",
    		version ='2016-05-19')
 	toneresult = json.dumps(tone_analyzer.tone(text=te)['document_tone'])
 	return toneresult
